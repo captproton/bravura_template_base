64c425e5ce8fe0be4eca0113add7eea0eb8f20a2
captproton <carl@wdwhub.net>
2024-08-25 16:51:14 -0700

    CHANGE: add pagy

--- app/controllers/concerns/bravura_template_base/blog_controller_concern.rb
+++ app/controllers/concerns/bravura_template_base/blog_controller_concern.rb
@@ -2,64 +2,51 @@
 module BravuraTemplateBase
   module BlogControllerConcern
     extend ActiveSupport::Concern
-
-    included do
-      before_action :load_settings_and_presenter
-      before_action :set_view_strategy
-      before_action :set_current_account
-    end
+    include BravuraTemplateBase::BaseControllerConcern
+    include Pagy::Backend  # Add this line to include Pagy
 
     def index
-      load_index_data
+      load_posts_data
       render_with_strategy :index
     end
 
     def show
-      load_show_data
+      load_post_data
       render_with_strategy :show
+    rescue ActiveRecord::RecordNotFound
+      render_not_found
+    end
+
+    def category
+      load_category_data
+      render_with_strategy :category
+    rescue ActiveRecord::RecordNotFound
+      render_not_found
+    end
+
+    def tag
+      load_tag_data
+      render_with_strategy :tag
     rescue ActiveRecord::RecordNotFound
       render_not_found
     end
 
     private
 
-    def load_index_data
-      # Override this method in the main controller to add more data
+    def load_posts_data
+      # Override this method in the main controller to load posts data
     end
 
-    def load_show_data
-      # Override this method in the main controller to load the post and related data
+    def load_post_data
+      # Override this method in the main controller to load specific post data
     end
 
-    def set_current_account
-      current_account = Current.account
-      @current_account = current_account
-    end
-    def set_view_strategy
-      current_settings = GuaranteedSettingService.for_account(current_account)
-      @view_strategy = BravuraTemplateBase::ViewStrategyFactory.create_for(
-        settings: current_settings,
-        template_name: current_settings.get("design.template")
-      )
+    def load_category_data
+      # Override this method in the main controller to load category-specific posts
     end
 
-    def load_settings_and_presenter
-      @settings ||= GuaranteedSettingService.for_account(current_account)
-      @presenter = BravuraTemplateBase::PresenterFactory.create(@settings)
-    end
-
-    def render_with_strategy(action)
-      template = @view_strategy.template_for(action)
-      layout = @view_strategy.layout
-      Rails.logger.debug "Rendering template: #{template}, layout: #{layout}"
-      render template: template, layout: layout
-    rescue => e
-      Rails.logger.error "Error in render_with_strategy: #{e.message}"
-      raise
-    end
-
-    def render_not_found
-      render template: "shared/not_found", status: :not_found
+    def load_tag_data
+      # Override this method in the main controller to load tag-specific posts
     end
   end
 end
